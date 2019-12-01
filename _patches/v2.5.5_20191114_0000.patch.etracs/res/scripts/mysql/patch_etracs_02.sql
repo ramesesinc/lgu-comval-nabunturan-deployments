e10a54eaae10b3fab49e324ffdc7acabe9b3e2c4
worgflores <worgflores@gmail.com>
2019-12-01 16:32:31 +0800

    updated

---
+++ _patches/v2.5.5_20191114_0000.patch.etracs/res/scripts/mysql/patch_etracs_02.sql
@@ -0,0 +1,247 @@
+drop table if exists sync_data_forprocess;
+drop table if exists sync_data_pending;
+drop table if exists sync_data;
+
+CREATE TABLE `sync_data` (
+  `objid` varchar(50) NOT NULL,
+  `parentid` varchar(50) NOT NULL,
+  `refid` varchar(50) NOT NULL,
+  `reftype` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `orgid` varchar(50) DEFAULT NULL,
+  `remote_orgid` varchar(50) DEFAULT NULL,
+  `remote_orgcode` varchar(20) DEFAULT NULL,
+  `remote_orgclass` varchar(20) DEFAULT NULL,
+  `dtfiled` datetime NOT NULL,
+  `idx` int(11) NOT NULL,
+  `sender_objid` varchar(50) DEFAULT NULL,
+  `sender_name` varchar(150) DEFAULT NULL,
+  `refno` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_sync_data_refid` (`refid`),
+  KEY `ix_sync_data_reftype` (`reftype`),
+  KEY `ix_sync_data_orgid` (`orgid`),
+  KEY `ix_sync_data_dtfiled` (`dtfiled`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+CREATE TABLE `sync_data_forprocess` (
+  `objid` varchar(50) NOT NULL,
+  PRIMARY KEY (`objid`),
+  CONSTRAINT `fk_sync_data_forprocess_sync_data` FOREIGN KEY (`objid`) REFERENCES `sync_data` (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+CREATE TABLE `sync_data_pending` (
+  `objid` varchar(50) NOT NULL,
+  `error` text,
+  `expirydate` datetime DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_expirydate` (`expirydate`),
+  CONSTRAINT `fk_sync_data_pending_sync_data` FOREIGN KEY (`objid`) REFERENCES `sync_data` (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+CREATE TABLE `faasannotation_faas` (
+  `objid` varchar(50) NOT NULL,
+  `parent_objid` varchar(50) NOT NULL,
+  `faas_objid` varchar(50) NOT NULL,
+  PRIMARY KEY (`objid`),
+  UNIQUE KEY `ux_parent_faas` (`parent_objid`,`faas_objid`),
+  KEY `ix_parent_objid` (`parent_objid`),
+  KEY `ix_faas_objid` (`faas_objid`),
+  CONSTRAINT `fk_faasannotationfaas_faas` FOREIGN KEY (`faas_objid`) REFERENCES `faas` (`objid`),
+  CONSTRAINT `fk_faasannotationfaas_faasannotation` FOREIGN KEY (`parent_objid`) REFERENCES `faasannotation` (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+
+
+alter table rptcertification add properties text
+;
+
+/* 255-03013 */
+alter table resection_item add newfaas_claimno varchar(25);
+alter table resection_item add faas_claimno varchar(25);
+
+
+/* 255-03015 */
+
+CREATE TABLE `rptcertification_online` (
+  `objid` varchar(50) NOT NULL,
+  `state` varchar(25) NOT NULL,
+  `reftype` varchar(25) NOT NULL,
+  `refid` varchar(50) NOT NULL,
+  `refno` varchar(50) NOT NULL,
+  `refdate` date NOT NULL,
+  `orno` varchar(25) DEFAULT NULL,
+  `ordate` date DEFAULT NULL,
+  `oramount` decimal(16,2) DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_state` (`state`),
+  KEY `ix_refid` (`refid`),
+  KEY `ix_refno` (`refno`),
+  KEY `ix_orno` (`orno`),
+  CONSTRAINT `fk_rptcertification_online_rptcertification` FOREIGN KEY (`objid`) REFERENCES `rptcertification` (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+
+CREATE TABLE `assessmentnotice_online` (
+  `objid` varchar(50) NOT NULL,
+  `state` varchar(25) NOT NULL,
+  `reftype` varchar(25) NOT NULL,
+  `refid` varchar(50) NOT NULL,
+  `refno` varchar(50) NOT NULL,
+  `refdate` date NOT NULL,
+  `orno` varchar(25) DEFAULT NULL,
+  `ordate` date DEFAULT NULL,
+  `oramount` decimal(16,2) DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_state` (`state`),
+  KEY `ix_refid` (`refid`),
+  KEY `ix_refno` (`refno`),
+  KEY `ix_orno` (`orno`),
+  CONSTRAINT `fk_assessmentnotice_online_assessmentnotice` FOREIGN KEY (`objid`) REFERENCES `assessmentnotice` (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+
+
+/*===============================================================
+**
+** FAAS ANNOTATION
+**
+===============================================================*/
+CREATE TABLE `faasannotation_faas` (
+  `objid` varchar(50) NOT NULL,
+  `parent_objid` varchar(50) NOT NULL,
+  `faas_objid` varchar(50) NOT NULL,
+  PRIMARY KEY (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+
+alter table faasannotation_faas 
+add constraint fk_faasannotationfaas_faasannotation foreign key(parent_objid)
+references faasannotation (objid)
+;
+
+alter table faasannotation_faas 
+add constraint fk_faasannotationfaas_faas foreign key(faas_objid)
+references faas (objid)
+;
+
+create index ix_parent_objid on faasannotation_faas(parent_objid)
+;
+
+create index ix_faas_objid on faasannotation_faas(faas_objid)
+;
+
+
+create unique index ux_parent_faas on faasannotation_faas(parent_objid, faas_objid)
+;
+
+alter table faasannotation modify column faasid varchar(50) null
+;
+
+
+
+-- insert annotated faas
+insert into faasannotation_faas(
+  objid, 
+  parent_objid,
+  faas_objid 
+)
+select 
+  objid, 
+  objid as parent_objid,
+  faasid as faas_objid 
+from faasannotation
+;
+
+
+
+/*============================================
+*
+*  LEDGER FAAS FACTS
+*
+=============================================*/
+INSERT INTO `sys_var` (`name`, `value`, `description`, `datatype`, `category`) 
+VALUES ('rptledger_rule_include_ledger_faases', '0', 'Include Ledger FAASes as rule facts', 'checkbox', 'LANDTAX')
+;
+
+INSERT INTO `sys_var` (`name`, `value`, `description`, `datatype`, `category`) 
+VALUES ('rptledger_post_ledgerfaas_by_actualuse', '0', 'Post by Ledger FAAS by actual use', 'checkbox', 'LANDTAX')
+;
+
+
+/* 255-03016 */
+
+/*================================================================
+*
+* RPTLEDGER REDFLAG
+*
+================================================================*/
+
+CREATE TABLE `rptledger_redflag` (
+  `objid` varchar(50) NOT NULL,
+  `parent_objid` varchar(50) NOT NULL,
+  `state` varchar(25) NOT NULL,
+  `caseno` varchar(25) NULL,
+  `dtfiled` datetime NULL,
+  `type` varchar(25) NOT NULL,
+  `finding` text,
+  `remarks` text,
+  `blockaction` varchar(25) DEFAULT NULL,
+  `filedby_objid` varchar(50) DEFAULT NULL,
+  `filedby_name` varchar(255) DEFAULT NULL,
+  `filedby_title` varchar(50) DEFAULT NULL,
+  `resolvedby_objid` varchar(50) DEFAULT NULL,
+  `resolvedby_name` varchar(255) DEFAULT NULL,
+  `resolvedby_title` varchar(50) DEFAULT NULL,
+  `dtresolved` datetime NULL,
+  PRIMARY KEY (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+create index ix_parent_objid on rptledger_redflag(parent_objid)
+;
+create index ix_state on rptledger_redflag(state)
+;
+create unique index ux_caseno on rptledger_redflag(caseno)
+;
+create index ix_type on rptledger_redflag(type)
+;
+create index ix_filedby_objid on rptledger_redflag(filedby_objid)
+;
+create index ix_resolvedby_objid on rptledger_redflag(resolvedby_objid)
+;
+
+alter table rptledger_redflag 
+add constraint fk_rptledger_redflag_rptledger foreign key (parent_objid)
+references rptledger(objid)
+;
+
+alter table rptledger_redflag 
+add constraint fk_rptledger_redflag_filedby foreign key (filedby_objid)
+references sys_user(objid)
+;
+
+alter table rptledger_redflag 
+add constraint fk_rptledger_redflag_resolvedby foreign key (resolvedby_objid)
+references sys_user(objid)
+;
+
+
+/* SUBLEDGER : add beneficiary info */
+
+alter table rptledger add beneficiary_objid varchar(50);
+create index ix_beneficiary_objid on rptledger(beneficiary_objid);
+
+
+/* COMPROMISE UPDATE */
+alter table rptcompromise_item add qtr int;
+
+
+alter table rptpayment drop foreign key rptpayment_cashreceipt;
